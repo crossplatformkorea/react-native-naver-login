3ee88f7fe9832f556dbfc4e8ab218146c4f838eb
hyochan <hyochan.org@gmail.com>
2017-09-12 11:28:53 +0900

    Fixed RNNaverLoginModule apis into 3 function. login, logout, getProfile.

--- android/src/main/java/com/dooboolab/naverlogin/RNNaverLoginModule.java
+++ android/src/main/java/com/dooboolab/naverlogin/RNNaverLoginModule.java
@@ -37,12 +37,25 @@ public class RNNaverLoginModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void naverLogout() {
+  public void getProfile(String accessToken, final Callback cb) {
+    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    asyncHttpClient.addHeader("Authorization", "Bearer " + accessToken);
+    asyncHttpClient.get(reactContext, "https://openapi.naver.com/v1/nid/me", new JsonHttpResponseHandler() {
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        super.onSuccess(statusCode, headers, response);
+        cb.invoke(null, response.toString());
+      }
+    });
+  }
+
+  @ReactMethod
+  public void logout() {
     mOAuthLoginModule.logout(reactContext);
   }
 
   @ReactMethod
-  public void naverLogin(String initials, final Callback cb) {
+  public void login(String initials, final Callback cb) {
     final Activity activity = getCurrentActivity();
     try {
       JSONObject jsonObject = new JSONObject(initials);
@@ -67,21 +80,18 @@ public class RNNaverLoginModule extends ReactContextBaseJavaModule {
                     long expiresAt = mOAuthLoginModule.getExpiresAt(reactContext);
                     String tokenType = mOAuthLoginModule.getTokenType(reactContext);
 
-                    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-                    asyncHttpClient.addHeader("Authorization", "Bearer " + accessToken);
-                    asyncHttpClient.get(reactContext, "https://openapi.naver.com/v1/nid/me", new JsonHttpResponseHandler() {
-                      @Override
-                      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        super.onSuccess(statusCode, headers, response);
-                        try {
-                          response.put("accessToken", accessToken);
-                          cb.invoke(null, response.toString());
-                        } catch (JSONException je) {
-                          Log.e(TAG, "JSONException: " + je);
-                          cb.invoke(je.getMessage(), null, null);
-                        }
-                      }
-                    });
+                    try {
+                      JSONObject response = new JSONObject();
+                      response.put("accessToken", accessToken);
+                      response.put("refreshToken", refreshToken);
+                      response.put("expiresAt", expiresAt);
+                      response.put("tokenType", tokenType);
+                      cb.invoke(null, response.toString());
+                    } catch (JSONException je) {
+                      Log.e(TAG, "JSONEXception: " + je.getMessage());
+                      cb.invoke(je.getMessage(), null);
+                    }
+
                   } else {
                     String errCode = mOAuthLoginModule.getLastErrorCode(reactContext).getCode();
                     String errDesc = mOAuthLoginModule.getLastErrorDesc(reactContext);

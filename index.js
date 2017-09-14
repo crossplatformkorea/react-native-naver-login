f71a6aed8db4a1eb790096bb900cf878d0e2e9d1
hyochan <hyochan.org@gmail.com>
2017-09-14 22:42:13 +0900

    Example files restructed. ios react-native link is not working.

--- index.js
+++ index.js
@@ -4,32 +4,3 @@ import { NativeModules, Platform } from 'react-native';
 const { RNNaverLogin } = NativeModules;
 
 export default RNNaverLogin;
-
-// const AuthAndroid = NativeModules.ReactAuthModule;
-const IosNaverLogin = NativeModules.RNNaverLogin;
-
-export async function startNaverLogin(keyObj) {
-  // keyObj = { kServiceAppUrlScheme, kConsumerKey, kConsumerSecret, kServiceAppName }
-
-  if (Platform.OS === 'ios') {
-    IosNaverLogin.startNaverAuth(keyObj, (err, token) => {
-
-      console.log(`\n\n   Token is fetched from iOS :: ${token} \n\n`);
-
-    });
-  } else {
-    // Android Part ..
-  }
-};
-
-export async function fetchProfile(token) {
-  if (Platform.OS === 'ios') {
-    IosNaverLogin.fetchProfile(token, (err, profile) => {
-
-      console.log(`\n\n   Profile is fetched from iOS :: ${profile} \n\n`);
-
-    });
-  } else {
-    // Android Part ..
-  }
-};

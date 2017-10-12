37bed87132479e4521cfa5cc4046f401861ba4f8
JongwooMoon <luna@LUNAui-MacBook-Pro.local>
2017-10-12 20:16:29 +0900

    Edit iOS Example Project

--- NaverLoginExample/src/components/pages/First/index.js
+++ NaverLoginExample/src/components/pages/First/index.js
@@ -6,59 +6,95 @@ import {
 } from 'react-native';
 
 import NativeButton from 'apsl-react-native-button';
+import { NaverLogin, getProfile } from 'react-native-naver-login';
 
 import Navbar from '../../shared/Navbar';
 import styles from './styles';
-import { naverLogin, getNaverProfile } from '../../../apis/AuthApi';
+// import { naverLogin, getNaverProfile } from '../../../apis/AuthApi';
+
+const initials = {
+  kConsumerKey: 'VN6WKGFQ3pJ0xBXRtlN9',
+  kConsumerSecret: 'AHBgzH9ZkM',
+  kServiceAppName: 'dooboolab',
+  kServiceAppUrlScheme: 'dooboolaburlscheme', // only for iOS
+};
+const naverInit = {
+  kConsumerKey: 'jyvqXeaVOVmV',
+  kConsumerSecret: '527300A0_COq1_XV33cf',
+  kServiceAppName: '네이버 아이디로 로그인하기',
+  kServiceAppUrlScheme: 'thirdparty20samplegame', // only for iOS
+};
 
 class Page extends Component {
   constructor(props) {
     super(props);
 
+    console.log('\n\n Initial Page :: src/components/pages/First/index.js \n\n');
+
     this.state = {
       isNaverLoggingin: false,
+      theToken: 'token has not fetched'
     };
   }
 
-  onNaverLogin = async() => {
-    console.log('Naver Login');
-    const initials = {
-      kConsumerKey: 'VN6WKGFQ3pJ0xBXRtlN9',
-      kConsumerSecret: 'AHBgzH9ZkM',
-      kServiceAppName: 'dooboolab',
-      kServiceAppUrlScheme: 'dooboolaburlscheme', // only for iOS
-    };
+  // onNaverLogin = async() => {
+  //   console.log('Naver Login');
+  //
+  //   try {
+  //     const result = await naverLogin(initials);
+  //     console.log('token: ' + result);
+  //
+  //     if (result) {
+  //       console.log('yes result');
+  //       const profileResult = await getNaverProfile(result);
+  //       console.log('profile');
+  //       console.log(profileResult);
+  //       if (profileResult.resultcode === '024') {
+  //         Alert.alert('로그인 실패', profileResult.message);
+  //         return;
+  //       }
+  //
+  //       result.profile = profileResult;
+  //       this.props.navigation.navigate('Second', {
+  //         result,
+  //         profileResult,
+  //       });
+  //     } else {
+  //       console.log('no result');
+  //     }
+  //
+  //   } catch (err) {
+  //     console.log('error');
+  //     console.log(err);
+  //   }
+  // }
 
-    try {
-      const result = await naverLogin(JSON.stringify(initials));
-      console.log('token: ' + result);
-
-      if (result) {
-        console.log('yes result');
-        const profileResult = await getNaverProfile(result);
-        console.log('profile');
-        console.log(profileResult);
-        if (profileResult.resultcode === '024') {
-          Alert.alert('로그인 실패', profileResult.message);
-          return;
-        }
-
-        result.profile = profileResult;
-        this.props.navigation.navigate('Second', {
-          result,
-          profileResult,
-        });
-      } else {
-        console.log('no result');
-      }
-
-    } catch (err) {
-      console.log('error');
-      console.log(err);
+  async fetchProfile() {
+    const profileResult = await getProfile(this.state.theToken);
+    console.log(profileResult);
+    if (profileResult.resultcode === '024') {
+      Alert.alert('로그인 실패', profileResult.message);
+      return;
     }
+    this.props.navigation.navigate('Second', {
+      profileResult,
+    });
+  }
+
+  async naverLoginStart() {
+    console.log('  naverLoginStart  ed');
+    NaverLogin.login(initials, (err, token) => {
+      console.log(`\n\n  Token is fetched  :: ${token} \n\n`);
+      this.setState({ theToken: token });
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
   }
 
   render() {
+    const { theToken } = this.state;
     return (
       <View style={ styles.container }>
         <View style={ styles.header }>
@@ -67,11 +103,20 @@ class Page extends Component {
         <View style={ styles.content }>
           <NativeButton
             isLoading={this.state.isNaverLoggingin}
-            onPress={this.onNaverLogin}
+            onPress={() => this.naverLoginStart()}
             activeOpacity={0.5}
             style={styles.btnNaverLogin}
             textStyle={styles.txtNaverLogin}
           >NAVER LOGIN</NativeButton>
+          <Text>{theToken}</Text>
+          <NativeButton
+            isLoading={this.state.isNaverLoggingin}
+            onPress={() => this.fetchProfile()}
+            activeOpacity={0.5}
+            style={styles.btnNaverLogin}
+            textStyle={styles.txtNaverLogin}
+          >Fetch Profile</NativeButton>
+
         </View>
       </View>
     );

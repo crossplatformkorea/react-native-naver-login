ded0e15bbcfcf26b1ec67dfed94c92f95c68f199
youngsoohan <amazingmobdev@gmail.com>
2019-11-21 18:26:33 +0900

    update example project

---
+++ NaverLoginExample/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Alert, SafeAreaView, StyleSheet, Button } from 'react-native';
+import { NaverLogin, getProfile } from '@react-native-seoul/naver-login';
+
+const initials = {
+  kConsumerKey: 'VC5CPfjRigclJV_TFACU',
+  kConsumerSecret: 'f7tLFw0AHn',
+  kServiceAppName: '테스트앱',
+  kServiceAppUrlScheme: 'testapp', // only for iOS
+};
+
+const App = () => {
+  const [naverToken, setNaverToken] = React.useState('');
+
+  const naverLogin = props => {
+    return new Promise((resolve, reject) => {
+      NaverLogin.login(props, (err, token) => {
+        console.log(`\n\n  Token is fetched  :: ${token} \n\n`);
+        setNaverToken(token);
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(token);
+      });
+    });
+  };
+
+  const naverLogout = () => {
+    NaverLogin.logout();
+  };
+
+  const getUserProfile = async () => {
+    const profileResult = await getProfile(naverToken);
+    if (profileResult.resultcode === '024') {
+      Alert.alert('로그인 실패', profileResult.message);
+      return;
+    }
+    console.log('profileResult', profileResult);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="네이버 아이디로 로그인하기"
+        onPress={() => naverLogin(initials)}
+      />
+      <Button title="로그아웃하기" onPress={naverLogout} />
+
+      {!!naverToken && (
+        <Button title="회원정보 가져오기" onPress={getUserProfile} />
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+});
+
+export default App;
--- NaverLoginExample/ios/NaverLoginExample/AppDelegate.m
+++ NaverLoginExample/ios/NaverLoginExample/AppDelegate.m
@@ -1,33 +1,25 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
-// 네이버 관련 세팅
-#import "NaverThirdPartyConstantsForApp.h"
-#import "NaverThirdPartyLoginConnection.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"NaverLoginExample"
+                                            initialProperties:nil];
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"NaverLoginExample"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -38,34 +30,13 @@
   return YES;
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  NSLog(@"\n\n\n  url scheme :: %@   \n\n\n .", url.scheme);
-
-  return [self handleWithUrl:url];
-}
-
-
-- (BOOL)handleWithUrl:(NSURL *)url {
-  NSLog(@"url : %@", url);
-  NSLog(@"url scheme : %@", url.scheme);
-  //NSLog(@"url scheme : %@", kServiceAppUrlScheme);
-  // NSLog(@"result - %d", [url.scheme isEqualToString:kServiceAppUrlScheme]);
-
-  
-      // 네이버앱으로부터 전달받은 url값을 NaverThirdPartyLoginConnection의 인스턴스에 전달
-      NaverThirdPartyLoginConnection *thirdConnection = [NaverThirdPartyLoginConnection getSharedInstance];
-      THIRDPARTYLOGIN_RECEIVE_TYPE resultType = [thirdConnection receiveAccessToken:url];
-
-      if (SUCCESS == resultType) {
-        NSLog(@"Getting auth code from NaverApp success!");
-      } else {
-        NSLog(@"  Error  ::  %u", resultType);
-        // 앱에서 resultType에 따라 실패 처리한다.
-        /*  SUCCESS = 0, PARAMETERNOTSET = 1, CANCELBYUSER = 2, NAVERAPPNOTINSTALLED = 3 , NAVERAPPVERSIONINVALID = 4,
-         .  OAUTHMETHODNOTSET = 5, INVALIDREQUEST = 6, CLIENTNETWORKPROBLEM = 7, UNAUTHORIZEDCLIENT = 8,
-         .  UNSUPPORTEDRESPONSETYPE = 9, NETWORKERROR = 10, UNKNOWNERROR = 11 */
-      }
-  return YES;
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 }
 
 @end

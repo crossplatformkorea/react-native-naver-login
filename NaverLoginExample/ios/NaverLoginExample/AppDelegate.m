0ad27ab0f31163292fa09827a8e24a10161663c7
youngsoohan <amazingmobdev@gmail.com>
2019-11-23 02:28:12 +0900

    update example  to be able to use naver app

--- NaverLoginExample/ios/NaverLoginExample/AppDelegate.m
+++ NaverLoginExample/ios/NaverLoginExample/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
 
 @implementation AppDelegate
 
@@ -27,6 +28,37 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [[NaverThirdPartyLoginConnection getSharedInstance] setIsNaverAppOauthEnable:YES];
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  NSLog(@"\n\n\n  url scheme :: %@   \n\n\n .", url.scheme);
+
+  return [self handleWithUrl:url];
+}
+
+
+- (BOOL)handleWithUrl:(NSURL *)url {
+  NSLog(@"url : %@", url);
+  NSLog(@"url scheme : %@", url.scheme);
+  //NSLog(@"url scheme : %@", kServiceAppUrlScheme);
+  // NSLog(@"result - %d", [url.scheme isEqualToString:kServiceAppUrlScheme]);
+
+  
+      // 네이버앱으로부터 전달받은 url값을 NaverThirdPartyLoginConnection의 인스턴스에 전달
+      NaverThirdPartyLoginConnection *thirdConnection = [NaverThirdPartyLoginConnection getSharedInstance];
+      THIRDPARTYLOGIN_RECEIVE_TYPE resultType = [thirdConnection receiveAccessToken:url];
+
+      if (SUCCESS == resultType) {
+        NSLog(@"Getting auth code from NaverApp success!");
+      } else {
+        NSLog(@"  Error  ::  %u", resultType);
+        // 앱에서 resultType에 따라 실패 처리한다.
+        /*  SUCCESS = 0, PARAMETERNOTSET = 1, CANCELBYUSER = 2, NAVERAPPNOTINSTALLED = 3 , NAVERAPPVERSIONINVALID = 4,
+         .  OAUTHMETHODNOTSET = 5, INVALIDREQUEST = 6, CLIENTNETWORKPROBLEM = 7, UNAUTHORIZEDCLIENT = 8,
+         .  UNSUPPORTEDRESPONSETYPE = 9, NETWORKERROR = 10, UNKNOWNERROR = 11 */
+      }
   return YES;
 }
 

0f13e38bdb557838fb2330bcc12b9690aa3690e7
Shinpei <react-native@kakao.com>
2020-01-20 16:56:18 +0900

    Fix: response must have all of token, expiresAt (#46)

--- index.ts
+++ index.ts
@@ -8,19 +8,37 @@ export interface ICallback<T> {
   (error: Error | undefined, result: T | undefined): void;
 }
 
+export interface TokenResponse {
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: string;
+  tokenType: string;
+}
+
+export interface GetProfileResponse {
+  resultcode: string;
+  message: string;
+  response: {
+    id: string;
+    profile_image: string | null;
+    email: string;
+    name: string;
+    birthday: string | null;
+  };
+}
 
 export interface ConfigParam {
     kConsumerKey: string
     kConsumerSecret: string
     kServiceAppName: string
-    
+
     /** Only for iOS */
     kServiceAppUrlScheme?: string
 }
 
 const NaverLoginIos = {
-  login(param: ConfigParam, callback: ICallback<string>) {
-    IosNaverLogin.login(JSON.stringify(param), callback);
+  login(param: ConfigParam, callback: ICallback<TokenResponse>) {
+    IosNaverLogin.login(param, callback);
   },
   logout() {
     IosNaverLogin.logout();
@@ -28,15 +46,15 @@ const NaverLoginIos = {
 };
 
 const RNNaverLoginAndr = {
-  login(param: ConfigParam, callback: ICallback<string>) {
-    RNNaverLogin.login(JSON.stringify(param), callback);
+  login(param: ConfigParam, callback: ICallback<TokenResponse>) {
+    RNNaverLogin.login(param, callback);
   },
   logout() {
     RNNaverLogin.logout();
   },
 };
 
-export const getProfile = (token: string): Promise<Response> => {
+export const getProfile = (token: string): Promise<GetProfileResponse> => {
   return fetch('https://openapi.naver.com/v1/nid/me', {
     method: 'GET',
     headers: {

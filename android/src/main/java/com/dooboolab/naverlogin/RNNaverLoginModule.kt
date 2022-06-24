c4cb56dc115195850ce7594485afb7f355e94124
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-06-24 17:05:18 +0900

    Naver Login Module: java to kotlin

---
+++ android/src/main/java/com/dooboolab/naverlogin/RNNaverLoginModule.kt
@@ -0,0 +1,93 @@
+package com.dooboolab.naverlogin
+
+import android.util.Log
+import com.facebook.react.bridge.*
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Callback
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
+
+class RNNaverLoginModule(
+    private val reactContext: ReactApplicationContext?
+) : ReactContextBaseJavaModule(reactContext) {
+    private var isLoginAllowed = false
+
+    override fun getName() = "RNNaverLogin"
+
+    @ReactMethod
+    fun logout() {
+        NaverIdLoginSDK.logout()
+    }
+
+    @ReactMethod
+    fun logoutWithCallback(callback: Callback) {
+        runCatching {
+            NaverIdLoginSDK.logout()
+            callback(null, true)
+        }.onFailure {
+            callback(it.message, null)
+        }
+    }
+
+    @ReactMethod
+    fun login(initials: ReadableMap, callback: Callback) {
+        isLoginAllowed = true
+
+        runCatching {
+            NaverIdLoginSDK.initialize(
+                reactContext
+                    ?: throw IllegalStateException("reactContext is not initialized: $reactContext"),
+                initials.getString("kConsumerKey"),
+                initials.getString("kConsumerSecret"),
+                initials.getString("kServiceAppName")
+            )
+            UiThreadUtil.runOnUiThread {
+                NaverIdLoginSDK.authenticate(reactContext, getAuthCallback(callback))
+            }
+        }.onFailure {
+            Log.e("Naver Login Error", "Error occured: $it", it)
+        }
+    }
+
+    private fun getAuthCallback(callback: Callback) = object : OAuthLoginCallback {
+        override fun onError(errorCode: Int, message: String) {
+            val errorMessage =
+                "errCode: ${NaverIdLoginSDK.getLastErrorCode().code}, errDesc: ${NaverIdLoginSDK.getLastErrorDescription()}"
+            Log.e(TAG, errorMessage)
+            if (isLoginAllowed) {
+                callback(errorMessage, null)
+                isLoginAllowed = false
+            }
+        }
+
+        override fun onFailure(httpStatus: Int, message: String) {
+            this.onFailure(httpStatus, message)
+        }
+
+        override fun onSuccess() {
+            runCatching {
+                val response = Arguments.createMap().run {
+                    putString("accessToken", NaverIdLoginSDK.getAccessToken())
+                    putString("refreshToken", NaverIdLoginSDK.getRefreshToken())
+                    putString("expiresAt", NaverIdLoginSDK.getExpiresAt().toString())
+                    putString("tokenType", NaverIdLoginSDK.getTokenType())
+                }
+
+                if (isLoginAllowed) {
+                    callback(null, response)
+                    isLoginAllowed = false
+                }
+            }.onFailure {
+                Log.e("Naver Login Error", "Error occured $it", it)
+                if (isLoginAllowed) {
+                    callback(it.message, null)
+                    isLoginAllowed = false
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "ReactNaverModule"
+    }
+}

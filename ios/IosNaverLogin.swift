da4d6a85464de3b65874f54c6176561c95559deb
mym0404 <mym0404@gmail.com>
2022-10-19 17:42:09 +0900

    [feat] Configure swift stub methods

--- ios/IosNaverLogin.swift
+++ ios/IosNaverLogin.swift
@@ -1,7 +1,13 @@
 import Foundation
+import NaverThirdPartyLogin
 
 @objc(IosNaverLogin)
 class IosNaverLogin: NSObject {
+	private let connection: NaverThirdPartyLoginConnection {
+		get {
+			return NaverThirdPartyLoginConnection.getSharedInstance()
+		}
+	}
 	
 	@objc
 	static func requiresMainQueueSetup() -> Bool{
@@ -11,5 +17,21 @@ class IosNaverLogin: NSObject {
 	@objc(multiply:withB:withResolver:withRejecter:)
 	func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
 		resolve(a*b)
+	}
+	
+	
+	@objc
+	func login(_ serviceUrlScheme: String, consumerKey: String, consumerSecret: String,
+			   appName: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock){
+		DispatchQueue.main.async {
+			NaverThirdPatyLoginConnection.
+		}
+	}
+	
+	@objc
+	func logout(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+		DispatchQueue.main.async {
+			
+		}
 	}
 }

660e2008b75f3e5ab172634cac987349ca80506e
mym0404 <mym0404@gmail.com>
2022-10-20 10:21:11 +0900

    [feat] Enhance example

--- NaverLoginExample/App.tsx
+++ NaverLoginExample/App.tsx
@@ -1,17 +1,21 @@
 import React, {useState} from 'react';
-import {Alert, SafeAreaView, Button, View, Text} from 'react-native';
-import NaverLogin, {NaverLoginResponse} from '@react-native-seoul/naver-login';
+import {SafeAreaView, Button, View, Text, ScrollView} from 'react-native';
+import NaverLogin, {
+  NaverLoginResponse,
+  GetProfileResponse,
+} from '@react-native-seoul/naver-login';
 
 const consumerKey = '';
 const consumerSecret = '';
 const appName = 'Hello';
-const serviceUrlScheme = '';
+const serviceUrlScheme = 'navertest';
 
 const App = () => {
   const [success, setSuccessResponse] =
     useState<NaverLoginResponse['successResponse']>();
   const [failure, setFailureResponse] =
     useState<NaverLoginResponse['failureResponse']>();
+  const [getProfileRes, setGetProfileRes] = useState<GetProfileResponse>();
 
   const login = async () => {
     const {failureResponse, successResponse} = await NaverLogin.login({
@@ -35,12 +39,12 @@ const App = () => {
   };
 
   const getProfile = async () => {
-    const profileResult = await NaverLogin.getProfile(success!.accessToken);
-    if (profileResult.resultcode === '024') {
-      Alert.alert('로그인 실패', profileResult.message);
-      return;
+    try {
+      const profileResult = await NaverLogin.getProfile(success!.accessToken);
+      setGetProfileRes(profileResult);
+    } catch (e) {
+      setGetProfileRes(undefined);
     }
-    console.log('profileResult', profileResult);
   };
 
   const deleteToken = async () => {
@@ -56,35 +60,52 @@ const App = () => {
   return (
     <SafeAreaView
       style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Button
-        title={success ? 'Logout' : 'Sign in with Naver'}
-        onPress={success ? logout : login}
-      />
-      <Gap />
-      {success ? (
-        <>
-          <Button title="Get Profile" onPress={getProfile} />
-          <Gap />
-        </>
-      ) : null}
-      {success ? (
-        <View style={{paddingHorizontal: 24}}>
-          <Button title="Delete Token" onPress={deleteToken} />
-          <Gap />
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>성공 응답</Text>
-          <Text>{JSON.stringify(success, null, 2)}</Text>
-        </View>
-      ) : null}
-      <Gap />
-      {failure ? (
-        <View style={{paddingHorizontal: 24}}>
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>실패 응답</Text>
-          <Text>{JSON.stringify(failure, null, 2)}</Text>
-        </View>
-      ) : null}
+      <ScrollView
+        style={{flex: 1}}
+        contentContainerStyle={{flexGrow: 1, padding: 24}}>
+        <Button title={'Login'} onPress={login} />
+        <Gap />
+        <Button title={'Logout'} onPress={logout} />
+        <Gap />
+        {success ? (
+          <>
+            <Button title="Get Profile" onPress={getProfile} />
+            <Gap />
+          </>
+        ) : null}
+        {success ? (
+          <View>
+            <Button title="Delete Token" onPress={deleteToken} />
+            <Gap />
+            <ResponseJsonText name={'Success'} json={success} />
+          </View>
+        ) : null}
+        <Gap />
+        {failure ? <ResponseJsonText name={'Failure'} json={failure} /> : null}
+        <Gap />
+        {getProfileRes ? (
+          <ResponseJsonText name={'GetProfile'} json={getProfileRes} />
+        ) : null}
+      </ScrollView>
     </SafeAreaView>
   );
 };
 const Gap = () => <View style={{marginTop: 24}} />;
+const ResponseJsonText = ({json = {}, name}: {json?: object; name: string}) => (
+  <View
+    style={{
+      padding: 12,
+      borderRadius: 16,
+      borderWidth: 1,
+      backgroundColor: '#242c3d',
+    }}>
+    <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+      {name}
+    </Text>
+    <Text style={{color: 'white', fontSize: 13, lineHeight: 20}}>
+      {JSON.stringify(json, null, 4)}
+    </Text>
+  </View>
+);
 
 export default App;

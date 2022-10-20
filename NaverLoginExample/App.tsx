75b3feb427c60e856ee8e3b45638f98816bd22d7
mym0404 <mym0404@gmail.com>
2022-10-20 16:45:03 +0900

    style: Fix Example project lint

--- NaverLoginExample/App.tsx
+++ NaverLoginExample/App.tsx
@@ -3,26 +3,48 @@ import type {
   GetProfileResponse,
   NaverLoginResponse,
 } from '@react-native-seoul/naver-login';
+import NaverLogin from '@react-native-seoul/naver-login';
+import type {ReactElement} from 'react';
 import React, {useState} from 'react';
 
-import NaverLogin from '@react-native-seoul/naver-login';
+const Gap = (): ReactElement => <View style={{marginTop: 24}} />;
+const ResponseJsonText = ({
+  json = {},
+  name,
+}: {
+  json?: object;
+  name: string;
+}): ReactElement => (
+  <View
+    style={{
+      padding: 12,
+      borderRadius: 16,
+      borderWidth: 1,
+      backgroundColor: '#242c3d',
+    }}>
+    <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+      {name}
+    </Text>
+    <Text style={{color: 'white', fontSize: 13, lineHeight: 20}}>
+      {JSON.stringify(json, null, 4)}
+    </Text>
+  </View>
+);
 
 const consumerKey = '';
 const consumerSecret = '';
 const appName = 'Hello';
 const serviceUrlScheme = 'navertest';
 
-const App = () => {
-  const [success, setSuccessResponse] = useState<
-    NaverLoginResponse['successResponse']
-  >();
+const App = (): ReactElement => {
+  const [success, setSuccessResponse] =
+    useState<NaverLoginResponse['successResponse']>();
 
-  const [failure, setFailureResponse] = useState<
-    NaverLoginResponse['failureResponse']
-  >();
+  const [failure, setFailureResponse] =
+    useState<NaverLoginResponse['failureResponse']>();
   const [getProfileRes, setGetProfileRes] = useState<GetProfileResponse>();
 
-  const login = async () => {
+  const login = async (): Promise<void> => {
     const {failureResponse, successResponse} = await NaverLogin.login({
       appName,
       consumerKey,
@@ -33,7 +55,7 @@ const App = () => {
     setFailureResponse(failureResponse);
   };
 
-  const logout = async () => {
+  const logout = async (): Promise<void> => {
     try {
       await NaverLogin.logout();
       setSuccessResponse(undefined);
@@ -44,7 +66,7 @@ const App = () => {
     }
   };
 
-  const getProfile = async () => {
+  const getProfile = async (): Promise<void> => {
     try {
       const profileResult = await NaverLogin.getProfile(success!.accessToken);
       setGetProfileRes(profileResult);
@@ -53,7 +75,7 @@ const App = () => {
     }
   };
 
-  const deleteToken = async () => {
+  const deleteToken = async (): Promise<void> => {
     try {
       await NaverLogin.deleteToken();
       setSuccessResponse(undefined);
@@ -66,12 +88,10 @@ const App = () => {
 
   return (
     <SafeAreaView
-      style={{alignItems: 'center', justifyContent: 'center', flex: 1}}
-    >
+      style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
       <ScrollView
         style={{flex: 1}}
-        contentContainerStyle={{flexGrow: 1, padding: 24}}
-      >
+        contentContainerStyle={{flexGrow: 1, padding: 24}}>
         <Button title={'Login'} onPress={login} />
         <Gap />
         <Button title={'Logout'} onPress={logout} />
@@ -99,23 +119,5 @@ const App = () => {
     </SafeAreaView>
   );
 };
-const Gap = () => <View style={{marginTop: 24}} />;
-const ResponseJsonText = ({json = {}, name}: {json?: object; name: string}) => (
-  <View
-    style={{
-      padding: 12,
-      borderRadius: 16,
-      borderWidth: 1,
-      backgroundColor: '#242c3d',
-    }}
-  >
-    <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
-      {name}
-    </Text>
-    <Text style={{color: 'white', fontSize: 13, lineHeight: 20}}>
-      {JSON.stringify(json, null, 4)}
-    </Text>
-  </View>
-);
 
 export default App;

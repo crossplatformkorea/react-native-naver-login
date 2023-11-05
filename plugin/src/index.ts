17e34c4f67b7a5256b42df0c4311d832bf55aeeb
KiYoon Kim <59464111+kkiriky@users.noreply.github.com>
2023-11-05 18:58:13 +0900

    feat: expo config plugin
    이 PR은 React Native 애플리케이션에 네이버 로그인을 통합하기 위해 설계된 Expo Config Plugin을 지원합니다. 필요한 plist 항목과 AppDelegate 수정 사항을 자동으로 구성함으로써, 플러그인은 설정 프로세스를 간소화합니다. 
    
    #### 변경 사항
    - `Info.plist`: 네이버 로그인을 위해 필요한 URL scheme과 LSApplicationQueriesSchemes를 설정합니다.
    - `AppDelegate`: Objective-C 앱 델리게이트에 네이버 로그인 처리를 위한 링킹 로직을 추가합니다.
    - 예외 처리: Swift 기반 AppDelegate 파일은 현재 지원하지 않으며, 이에 대한 경고 메시지를 추가했습니다.
    
    #### 배경
    네이버 로그인은 한국에서 널리 사용되는 인증 방식 중 하나이며, 이를 애플리케이션에 통합하는 것은 사용자의 편리성을 대폭 향상시킵니다. 이 플러그인 업데이트는 개발자가 네이버 로그인 기능을 더 쉽고 빠르게 구현할 수 있도록 지원하기 위해 만들어졌습니다.
    
    #### 주의 사항
    - 이 업데이트는 Objective-C 기반의 AppDelegate 파일만 지원합니다. Swift로 작성된 AppDelegate 파일에 대한 지원은 향후 업데이트에서 고려될 예정입니다.
    - `urlScheme`의 유효성을 확인하는 추가적인 검증 로직은 현재 구현되지 않았습니다. 개발자는 유효한 `urlScheme`을 제공해야 합니다.
    
    #### 테스트
    변경 사항은 로컬 환경에서 기본적인 테스트를 거쳐 확인되었으나, 추가적인 테스트가 필요할 수 있습니다. 테스트는 각자의 환경에서 진행하고 결과를 공유해주시기 바랍니다.

---
+++ plugin/src/index.ts
@@ -0,0 +1,113 @@
+import {
+  ConfigPlugin,
+  withAppDelegate,
+  withInfoPlist,
+  WarningAggregator,
+} from "@expo/config-plugins";
+
+interface NaverLoginPluginProps {
+  urlScheme: string;
+}
+
+const NAVER_LAUNCH_SERVICE_QUERIES_SCHEMES = [
+  "naversearchapp",
+  "naversearchthirdlogin",
+];
+
+const NAVER_HEADER_IMPORT_STRING =
+  "#import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>";
+
+const createNaverLinkingString = (
+  urlScheme: string
+) => `if ([url.scheme isEqualToString:@"${urlScheme}"]) {
+    return [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url options:options];
+  }`;
+
+const modifyInfoPlist: ConfigPlugin<NaverLoginPluginProps> = (
+  config,
+  { urlScheme }
+) => {
+  return withInfoPlist(config, (config) => {
+    if (!Array.isArray(config.modResults.LSApplicationQueriesSchemes)) {
+      config.modResults.LSApplicationQueriesSchemes =
+        NAVER_LAUNCH_SERVICE_QUERIES_SCHEMES;
+    } else {
+      NAVER_LAUNCH_SERVICE_QUERIES_SCHEMES.forEach((scheme) => {
+        if (!config.modResults.LSApplicationQueriesSchemes?.includes(scheme)) {
+          config.modResults.LSApplicationQueriesSchemes?.push(scheme);
+        }
+      });
+    }
+
+    if (!Array.isArray(config.modResults.CFBundleURLTypes)) {
+      config.modResults.CFBundleURLTypes = [];
+    }
+
+    const isExist = config.modResults.CFBundleURLTypes.some(
+      (item) =>
+        item.CFBundleURLName === "naver" &&
+        item.CFBundleURLSchemes.includes(urlScheme)
+    );
+
+    if (!isExist) {
+      config.modResults.CFBundleURLTypes.push({
+        CFBundleURLName: "naver",
+        CFBundleURLSchemes: [urlScheme],
+      });
+    }
+
+    return config;
+  });
+};
+
+const modifyContents = (contents: string, urlScheme: string) => {
+  if (!contents.includes(NAVER_HEADER_IMPORT_STRING)) {
+    contents = contents.replace(
+      "#import <React/RCTLinkingManager.h>",
+      `#import <React/RCTLinkingManager.h>
+${NAVER_HEADER_IMPORT_STRING}`
+    );
+  }
+
+  const naverLinkingString = createNaverLinkingString(urlScheme);
+
+  if (!contents.includes(naverLinkingString)) {
+    contents = contents.replace(
+      "options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {",
+      `options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  ${naverLinkingString}
+`
+    );
+  }
+
+  return contents;
+};
+
+const modifyAppDelegate: ConfigPlugin<NaverLoginPluginProps> = (
+  config,
+  { urlScheme }
+) => {
+  return withAppDelegate(config, async (config) => {
+    if (["objc", "objcpp"].includes(config.modResults.language)) {
+      config.modResults.contents = modifyContents(
+        config.modResults.contents,
+        urlScheme
+      );
+    } else {
+      WarningAggregator.addWarningIOS(
+        "withNaverLogin",
+        "Swift AppDelegate files are not supported yet."
+      );
+    }
+
+    return config;
+  });
+};
+
+const withNaverLogin: ConfigPlugin<NaverLoginPluginProps> = (config, props) => {
+  config = modifyInfoPlist(config, props);
+  config = modifyAppDelegate(config, props);
+  return config;
+};
+
+export default withNaverLogin;

898f0ea3ba4c875e95318e5c1a19910d4f617757
ollehkt <97086762+ollehkt@users.noreply.github.com>
2025-04-25 19:39:12 +0900

    fix: function type in android RNNaverLoginModule.kt file (#216)
    * fix: fix function type in android RNNaverLoginModule.kt file
    * chore: remove extra space

--- android/src/main/java/com/dooboolab/naverlogin/RNNaverLoginModule.kt
+++ android/src/main/java/com/dooboolab/naverlogin/RNNaverLoginModule.kt
@@ -18,11 +18,12 @@ class RNNaverLoginModule(reactContext: ReactApplicationContext) : ReactContextBa
     override fun getName() = "RNNaverLogin"
 
     @ReactMethod
-    fun logout(promise: Promise) =
+    fun logout(promise: Promise) {
         UiThreadUtil.runOnUiThread {
             callLogout()
             promise.safeResolve(null)
         }
+    }
 
     private fun callLogout() =
         try {
@@ -46,7 +47,7 @@ class RNNaverLoginModule(reactContext: ReactApplicationContext) : ReactContextBa
     }
 
     @ReactMethod
-    fun login(promise: Promise) =
+    fun login(promise: Promise) {
         UiThreadUtil.runOnUiThread {
             loginPromise = promise
             if (currentActivity == null) {
@@ -80,9 +81,10 @@ class RNNaverLoginModule(reactContext: ReactApplicationContext) : ReactContextBa
                 onLoginFailure(je.localizedMessage)
             }
         }
+    }
 
     @ReactMethod
-    fun deleteToken(promise: Promise) =
+    fun deleteToken(promise: Promise) {
         UiThreadUtil.runOnUiThread {
             NidOAuthLogin().callDeleteTokenApi(
                 object : OAuthLoginCallback {
@@ -100,6 +102,7 @@ class RNNaverLoginModule(reactContext: ReactApplicationContext) : ReactContextBa
                 },
             )
         }
+    }
 
     companion object {
         private var loginPromise: Promise? = null
